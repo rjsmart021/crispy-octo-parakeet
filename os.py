6423b005f952f5a4fa9de9640baa44f4c7db08b7
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-09-17 23:03:02 -0500

    Update os.py

--- os.py
+++ os.py
@@ -4,10 +4,11 @@ import os
         # first we want the function to print all files
         return os.listdir(path)
         #use this function to access files deeper into a decision tree
-        return os. walk(path)
+        return os.walk(path)
         #generate all path files
         return pathlib
-        files = [f for f in pathlib.Path().glob("/sys/*.log")]
-        print(files)
-        # use this function to return a generator.
-        return os. scandir()
+            try : 
+                print(os.ttyname(r))  
+                except OSError as error : 
+                    print(error) 
+                    print("File descriptor is not associated with any terminal device") 
